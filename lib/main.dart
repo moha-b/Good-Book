7ea81983349d8ab1306a51bae81e0e7a8c7ec888
hab <Mohabzax4@gmail.com>
2023-01-20 19:57:05 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const GoodBook());
+}
+
+class GoodBook extends StatelessWidget {
+  const GoodBook({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home:
+    );
+  }
+}
